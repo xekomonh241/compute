9d29fbbe872595863be88173283ad487a918e0c2
Nguyễn Đức Mạnh <67572602+xekomonh241@users.noreply.github.com>
2022-11-29 21:07:07 +0700

    .

---
+++ Paycompute.Entity/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paycompute.Entity
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string EmployeeNo { get; set; }
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(200)]
+        public string FullName { get; set; }
+        [MaxLength(50)]
+        public string MidleName { get; set; }
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime DateJoined { get; set; }
+        public string Phone { get; set; }
+        public string Designation { get; set; }
+        public string Email { get; set; }
+        [Required, MaxLength(50)]
+        public string NationalInsuranceNo { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public StudentLoan StudentLoan { get; set; }
+        public UnionMember UnionMember { get; set; }
+        [Required, MaxLength(150)]
+        public string Address { get; set; }
+        [Required, MaxLength(50)]
+        public string City { get; set; }
+        [Required, MaxLength(50)]
+        public string Postcode { get; set; }
+      
+        public IEnumerable<PaymentRecord> PaymentRecords { get; set; }
+    }
+}
---
+++ Paycomputer.service/IEmployeeService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Paycompute.Entity;
+
+
+namespace Paycomputer.service
+{
+    public interface IEmployeeService
+    {
+        Task CreateAsync(Employee newEmployee);
+        Employee GetById(int employeeId);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int id);
+        Task Delete(int employeeId);
+        decimal UnionFees(int id);
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount);
+        IEnumerable<SelectListItem> GetAllEmployeesForPayroll();
+        IEnumerable<Employee> GetAll();
+    }
+}
